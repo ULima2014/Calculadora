f423af61fb4be603bedc9e91b3994af9a7eb32f5
Bryan <B@BC2M2.mshome.net>
2014-10-26 02:03:51 -0500

    Commit 1

---
+++ src/java/edu/ulima/bean/AlumnoDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ulima.bean;
+
+/**
+ *
+ * @author B
+ */
+public class AlumnoDAO {
+    
+}
